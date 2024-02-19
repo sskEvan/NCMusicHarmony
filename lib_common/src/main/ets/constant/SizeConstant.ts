ece4f8e2883a108ff22dda4b04f4a51249f475cc
ssk <senkun.shi@get88.cn>
2024-02-19 14:23:33 +0800

    first commit

---
+++ lib_common/src/main/ets/constant/SizeConstant.ts
@@ -0,0 +1,62 @@
+export class SizeConstant {
+  // 标题栏高度
+  static readonly APP_BAR_HEIGHT = 38
+  // TabLayout高度
+  static readonly TAB_LAYOUT_HEIGHT = 38
+  // TabLayout指示器宽度
+  static readonly TAB_LAYOUT_INDICATOR_WIDTH = 14
+  // TabLayout指示器高度
+  static readonly TAB_LAYOUT_INDICATOR_HEIGHT = 2.5
+  // 底部导航栏高度
+  static readonly NAV_BAR_HEIGHT = 48
+  // 音乐播放栏高度
+  static readonly MUSIC_PLAY_BAR_HEIGHT = 44
+
+
+  // 文本大小相关
+  // 小号文本
+  static readonly TEXT_S = 10
+  // 中号
+  static readonly TEXT_M = 12
+  // 大号文本
+  static readonly TEXT_L = 14
+  // X大号文本
+  static readonly TEXT_XL = 16
+  // XX大号文本
+  static readonly TEXT_XXL = 18
+
+
+  // 图标大小相关
+  // 小号图标
+  static readonly ICON_S = 13
+  // 中号图标
+  static readonly ICON_M = 16
+  // 大号图标
+  static readonly ICON_L = 19
+  // X大号图标
+  static readonly ICON_XL = 24
+  // XX大号图标
+  static readonly ICON_XXL = 32
+
+  // 圆角大小相关
+  // 小号圆角
+  static readonly RADIUS_S = 3
+  // 中号圆角
+  static readonly RADIUS_M = 5
+  // 大号圆角
+  static readonly RADIUS_L = 14
+
+  // 间距相关
+  // 小号间距
+  static readonly SPACE_S = 5
+  // 中号间距
+  static readonly SPACE_M = 10
+  // 大号间距
+  static readonly SPACE_L = 14
+  // X大号间距
+  static readonly SPACE_XL = 18
+  // XX大号间距
+  static readonly SPACE_XXL = 26
+}
+
+
